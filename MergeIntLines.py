9902823c0e5c47eda64fc0de07ff5b1361d9df36
cramos95 <livestrong095@yahoo.com>
2024-06-26 20:51:16 -0500

    Update MergeIntLines.py

--- MergeIntLines.py
+++ MergeIntLines.py
@@ -1,7 +1,7 @@
 # Python Script: Merge Intersecting Lines
 # Author: Chad Ramos
 # This script tool merges input line features where they intersect (within a given search distance)
-# based on the problem and solution at https://community.esri.com/t5/arcgis-pro-questions/how-to-merge-intersecting-line-segments/m-p/1066989#M41870
+# based on the problem and solution at https://community.esri.com/t5/arcgis-pro-questions/how-to-merge-intersecting-line-segments/m-p/1066880
 # This does not preserve any attribute data from the input features
 
 # import modules
